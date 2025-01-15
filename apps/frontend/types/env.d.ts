a88a1a1f3926a62fa706ffede05fc0f443cd5d7c
xonery <kilian.houpeurt@gmail.com>
2025-01-15 15:04:28 +0100

    chore: remove unused features from template

--- apps/frontend/types/env.d.ts
+++ apps/frontend/types/env.d.ts
@@ -5,9 +5,6 @@ declare global {
       BACKEND_URL: string;
       FRONTEND_URL: string;
       STORAGE_SECRET: string;
-
-      OLLAMA_API_URL: string;
-      OLLAMA_API_MODEL: string;
     }
   }
 }
